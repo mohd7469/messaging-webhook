a44c68891662e4ade2aba2556b2850a99141fe85
Awais Karni <awais3227443@gmail.com>
2024-08-11 19:54:01 +0500

    Add files via upload

---
+++ twilio/statusCallback.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res) => {
+  // trigger on whatsapp msg sent, delivered or seen
+
+  // const messageSid = req.body.MessageSid;
+  // const messageStatus = req.body.MessageStatus;
+
+  // console.log(`Message SID: ${messageSid}`);
+  // console.log(`Message Status: ${messageStatus}`); // sent, delivered, seen
+
+  // do stuff here
+  res.sendStatus(200);
+};
